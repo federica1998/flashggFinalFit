4b701af3561ef4f3ae88217a03fb808dec69a80c
federica1998 <federica.deriggi@hotmail.it>
2024-12-02 17:37:21 +0100

    changing in run_sequence_breakdown_proc.sh

--- Combine/run_sequence_breakdown_proc.sh
+++ Combine/run_sequence_breakdown_proc.sh
@@ -34,8 +34,13 @@ if [[ $DR ]]; then
     DROPT=" --dryRun "
 fi
 
-fits=(    "ALT_0M" )
+#fits=("ALT_L1" "ALT_L1Zg" "ALT_0PH" )
 ext2=("GGH" "TTH" "VHHAD" "VBF" "VHMET" "VHLEP" )
+ext2=( "TTH" "VHMET"  "NoSyst")
+fits=("ALT_0M" )
+
+
+
 
 
 
@@ -43,23 +48,24 @@ ext2=("GGH" "TTH" "VHHAD" "VBF" "VHMET" "VHLEP" )
 if [[ $STEP == "txt" ]];then
    for fit in ${fits[*]}
    do 
-    combineCards.py Datacard_${fit}_NO_SYST.txt --ic=".*J_.*|.*RECO_PTH.*|.*VBFLIKEGGH.*" > Datacard_${fit}_GGH.txt
+    combineCards.py Datacard_${fit}.txt --ic=".*J_.*|.*RECO_PTH.*|.*VBFLIKEGGH.*"  -s  > Datacard_${fit}_GGH.txt
     awk '!/pdfindex/ || /.*J_.*|.*RECO_PTH.*|.*VBFLIKEGGH.*/' Datacard_${fit}_GGH.txt > test.txt && mv test.txt Datacard_${fit}_GGH.txt
 
-    combineCards.py Datacard_${fit}_NO_SYST.txt --ic=".*TTH.*" > Datacard_${fit}_TTH.txt
+    combineCards.py Datacard_${fit}.txt --ic=".*TTH.*" -s > Datacard_${fit}_TTH.txt
     awk '!/pdfindex/ || /.*TTH.*/' Datacard_${fit}_TTH.txt > test.txt && mv test.txt Datacard_${fit}_TTH.txt
 
-    combineCards.py Datacard_${fit}_NO_SYST.txt --ic=".*VHHAD.*" > Datacard_${fit}_VHHAD.txt
+    combineCards.py Datacard_${fit}.txt --ic=".*VHHAD.*"  -s > Datacard_${fit}_VHHAD.txt
     awk '!/pdfindex/ || /.*VHHAD.*/' Datacard_${fit}_VHHAD.txt > test.txt && mv test.txt Datacard_${fit}_VHHAD.txt
 
-    combineCards.py Datacard_${fit}_NO_SYST.txt --ic=".*VBFTOPO_ACVBF.*|.*VBFTOPO_ACGGH.*" > Datacard_${fit}_VBF.txt
+    combineCards.py Datacard_${fit}.txt --ic=".*VBFTOPO_ACVBF.*|.*VBFTOPO_ACGGH.*"  -s > Datacard_${fit}_VBF.txt
     awk '!/pdfindex/ || /.*VBFTOPO_ACVBF.*|.*VBFTOPO_ACGGH.*/' Datacard_${fit}_VBF.txt > test.txt && mv test.txt Datacard_${fit}_VBF.txt
 
-    combineCards.py Datacard_${fit}_NO_SYST.txt --ic=".*VH_MET.*" > Datacard_${fit}_VHMET.txt
+    combineCards.py Datacard_${fit}.txt --ic=".*VH_MET.*" -s  > Datacard_${fit}_VHMET.txt
     awk '!/pdfindex/ || /.*VH_MET.*/' Datacard_${fit}_VHMET.txt > test.txt && mv test.txt Datacard_${fit}_VHMET.txt
 
-    combineCards.py Datacard_${fit}_NO_SYST.txt --ic=".*_LEP_Tag.*" > Datacard_${fit}_VHLEP.txt
+    combineCards.py Datacard_${fit}.txt --ic=".*_LEP_Tag.*" -s  > Datacard_${fit}_VHLEP.txt
     awk '!/pdfindex/ || /.*_LEP_Tag.*/' Datacard_${fit}_VHLEP.txt > test.txt && mv test.txt Datacard_${fit}_VHLEP.txt
+
    done
 elif [[ $STEP == "t2w" ]]; then
   for fit in ${fits[*]}
@@ -104,7 +110,10 @@ elif [[ $STEP == "plot" ]]; then
         for fit in ${fits[*]}
         do
            string="runFits${fit}_TTH_${fit}/profile1D_syst_${fit}_TTH_CMS_zz4l_fai1.root:TTH:2 runFits${fit}_VBF_${fit}/profile1D_syst_${fit}_VBF_CMS_zz4l_fai1.root:VBF:3 runFits${fit}_VHHAD_${fit}/profile1D_syst_${fit}_VHHAD_CMS_zz4l_fai1.root:VHHAD:4  runFits${fit}_VHMET_${fit}/profile1D_syst_${fit}_VHMET_CMS_zz4l_fai1.root:VH-MET:9 runFits${fit}_VHLEP_${fit}/profile1D_syst_${fit}_VHLEP_CMS_zz4l_fai1.root:VH-LEP:46"
-           plot1DScan.py runFits${fit}_GGH_${fit}/profile1D_syst_${fit}_GGH_CMS_zz4l_fai1.root   --y-cut 30 --y-max 30 -o  plots/Breakdown_${fit} --POI CMS_zz4l_fai1 --main-label GGH --translate ../Plots/pois_fa3.json --others $string
+           plot1DScan.py runFits${fit}_GGH_${fit}/profile1D_syst_${fit}_GGH_CMS_zz4l_fai1.root   --y-cut 4 --y-max 4 -o  plots/Breakdown_${fit} --POI CMS_zz4l_fai1 --main-label GGH --translate ../Plots/pois_fa3.json --others $string
+        
+#           string="runFits${fit}_NoSyst_${fit}/profile1D_syst_${fit}_NoSyst_CMS_zz4l_fai1.root:NoSyst:2 "
+ #          plot1DScan.py runFits${fit}_${fit}/profile1D_syst_${fit}_CMS_zz4l_fai1.root   --y-cut 15 --y-max 15 -o  plots/Breakdown_Syst_${fit} --POI CMS_zz4l_fai1 --main-label Syst --translate ../Plots/pois_fa3.json --others $string
         done
     done
 else
